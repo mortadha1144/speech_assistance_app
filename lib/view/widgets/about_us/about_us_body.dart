73bf0a355320026ce458b13546c609954b43601e
Mortadha Naser <mortadha114@gmail.com>
2023-07-05 21:36:00 +0300

    updar about us contanct text

--- lib/view/widgets/about_us/about_us_body.dart
+++ lib/view/widgets/about_us/about_us_body.dart
@@ -64,7 +64,7 @@ class AboutBody extends StatelessWidget {
                   children: [
                     isAboutUs
                         ? const Text(
-                            'تابعنا على :',
+                            'للتواصل معنا :',
                             style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
